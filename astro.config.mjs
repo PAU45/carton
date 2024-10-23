871f483c57627cd2709de0199ab474a4e7cb8de3
PAU45 <paulo.melendez@tecsup.edu.pe>
2024-10-23 15:24:26 -0500

    noveno commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,4 +10,6 @@ export default defineConfig({
   output: 'server', // o 'hybrid'
   adapter: netlify(),
   integrations: [react(), tailwind()],
+  site: 'https://portafolio-paulo-melendez.com', // Reemplaza con la URL de tu sitio
+  outDir: './dist',
 });
